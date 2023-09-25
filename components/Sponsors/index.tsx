f7cc7cffb7d14d330385b9b0dbd53bddc1a742ab
James Teng <jamest.1402@gmail.com>
2023-09-25 23:34:07 +1000

    make it clickable

--- components/Sponsors/index.tsx
+++ components/Sponsors/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Sponsors = () => {
@@ -37,7 +38,9 @@ const Sponsors = () => {
       <div className="sm:flex justify-between">
         <div className="flex items-center">
           <img src="assets/csesoc_logo_white.svg" alt="CSESoc Logo" />
-          <img src="/flag/ollie_is_hiding.png" alt="Ollie" draggable="false" width={60} height={60} className="ml-10" />
+          <Link href="/flag/ollie_is_hiding.png" target="_blank">
+            <img src="/flag/ollie_is_hiding.png" alt="Ollie" draggable="false" width={60} height={60} className="ml-10" />
+          </Link>
         </div>
         <div className="flex flex-col max-w-[14rem] sm:mt-0 mt-10 font-light">
           <p className="mb-6">B03 CSE Building K17, UNSW csesoc@csesoc.org.au</p>
