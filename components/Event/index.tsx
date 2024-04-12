b236314c8d33566f9eb2334da9f6e9ea09a9d8bf
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-04-13 00:00:44 +1000

    CW2-3 Add events section (#4)
    * added events template
    
    * removed section
    
    * modified events to coordinate with navbar on click
    
    ---------
    
    Co-authored-by: Stuart Cutbush <stuart@Stuarts-MacBook-Pro.local>
    Co-authored-by: Derek Xu <xuderek2004@gmail.com>

--- components/Event/index.tsx
+++ components/Event/index.tsx
@@ -1,5 +1,19 @@
 const Event = () => {
-  return <section></section>;
+  return (
+    <section className="py-8 xl:px-24 sm:px-10 px-5" id="events">
+      <div className="text-center my-10">
+        <p className="text-[#3977F8] font-game text-xl">02</p>
+        <h1 className="font-bold text-6xl">EVENTS</h1>
+      </div>
+      <div className="flex items-center">
+        <p className="text-center text-3xl">
+          We run a wide-variety of events for fun, learning new skills and careers. For full
+          listings, check out the CSESoc Discord or our Facebook page!
+        </p>
+        <div className="bg-blue-500 w-4/5 h-96 ml-80">Placeholder</div>
+      </div>
+    </section>
+  );
 };
 
 export default Event;
