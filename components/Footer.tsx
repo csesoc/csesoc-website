9de4180a7ca435871aabdadff8268604b87be556
Saucy_Salad <andrewbruce222@gmail.com>
2024-04-16 01:45:05 +1000

    social media to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,42 @@
 import Link from 'next/link';
+//import SponsorLinks from './Sponsors/sponsorlinks';
+import { socialLinks } from '../public/data/data';
 
 const Footer = () => {
   return (
     <section>
-      <div className="sm:flex justify-between">
-        <div className="flex items-center">
-          <img src="assets/csesoc_logo_white.svg" alt="CSESoc Logo" />
-          <Link href="/flag/ollie_is_hiding.png" target="_blank" className="sm:hidden block">
+      <div className="sm:flex justify-between pb-10">
+        <div className="flex flex-col gap-10">
+          <div className="flex items-center">
+            <img src="assets/csesoc_logo_white.svg" alt="CSESoc Logo" />
+            <Link href="/flag/ollie_is_hiding.png" target="_blank" className="sm:hidden block">
+              <img
+                src="/flag/ollie_is_hiding.png"
+                alt="Ollie"
+                draggable="false"
+                width={60}
+                height={60}
+                className="ml-10"
+              />
+            </Link>
             <img
               src="/flag/ollie_is_hiding.png"
               alt="Ollie"
               draggable="false"
               width={60}
               height={60}
-              className="ml-10"
+              className="ml-10 sm:block hidden"
             />
-          </Link>
-          <img
-            src="/flag/ollie_is_hiding.png"
-            alt="Ollie"
-            draggable="false"
-            width={60}
-            height={60}
-            className="ml-10 sm:block hidden"
-          />
+          </div>
+          <div className="grid grid-cols-8 gap-4">
+          {socialLinks.map((item, index) => {
+            return (
+              <a key={index} className="" href={item.href}>
+                <img className="h-4" src={item.src} alt={item.alt} />
+              </a>
+            );
+          })}
+          </div>
         </div>
         <div className="flex flex-col max-w-[14rem] sm:mt-0 mt-10 font-light">
           <p className="mb-6">B03 CSE Building K17, UNSW csesoc@csesoc.org.au</p>
