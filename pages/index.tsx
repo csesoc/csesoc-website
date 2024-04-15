a6b15d6dece9d775c5f297c3e180586066456d43
Saucy_Salad <andrewbruce222@gmail.com>
2024-04-16 01:45:05 +1000

    social media to footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Sponsors from '@/components/Sponsors/index';
 import About from '@/components/About';
 import Event from '@/components/Event';
 import ResourcesAndContacts from '@/components/ResourcesAndContacts';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
