ec671b0568aae751816ec80af2a83523018eedeb
Chris Yoo <chris.yoo@safetyculture.io>
2024-05-30 18:33:24 +1000

    setup communication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Express } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import pingRoute from "./routes/ping";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 9000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use(pingRoute);
 
 app.listen(port, () => {
   console.log(`Server successfully started on port ${port}`);
--- frontend/src/pages/events.tsx
+++ frontend/src/pages/events.tsx
@@ -1,11 +1,21 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import axios from 'axios';
 
 export default function EventsPage() {
+  const pingBackend = async () => {
+    axios
+      .get(
+        `http://${process.env.NEXT_PUBLIC_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/ping`
+      )
+      .then((resp) => console.log(resp.data))
+      .catch((error) => console.log(error.message));
+  };
+
   return (
     <section className="flex flex-col min-h-screen justify-between py-8 xl:px-24 md:px-10 px-5 relative overflow-hidden">
       <Navbar />
-      <div>Events</div>
+      <button onClick={() => pingBackend()}>Ping backend</button>
       <Footer />
     </section>
   );
