43c6187792fa917993b494eaeb1b455f1134cddf
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-06-07 01:52:03 +1000

    added contact page

--- frontend/src/pages/contact-us.tsx
+++ frontend/src/pages/contact-us.tsx
@@ -1,6 +1,18 @@
+import Footer from '@/components/Footer';
+import Hamburger from '@/components/Hamburger';
+import Navbar from '@/components/Navbar';
+
 export default function ContactUs() {
-    return <div>
-        <h1>Hello world</h1>
-    </div>
-    
+  return (
+    <section className="flex flex-col min-h-screen justify-between py-8 xl:px-24 md:px-10 px-5 relative overflow-hidden">
+      <Navbar />
+      <section>
+        <p>
+          Follow us on social media to be notified of upcoming events and opportunities' or
+          something like that
+        </p>
+      </section>
+      <Footer />
+    </section>
+  );
 }
