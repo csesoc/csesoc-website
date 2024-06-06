7b1271b070f6b3971428bb96f6aa71dd1b5463ef
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-06-07 02:07:00 +1000

    added content for contact us with reference to https://media.csesoc.org.au/contact-us/

--- frontend/src/pages/contact-us.tsx
+++ frontend/src/pages/contact-us.tsx
@@ -7,9 +7,59 @@ export default function ContactUs() {
     <section className="flex flex-col min-h-screen justify-between py-8 xl:px-24 md:px-10 px-5 relative overflow-hidden">
       <Navbar />
       <section>
+        <img
+          style={{ padding: '10rem' }}
+          src="https://media.csesoc.org.au/content/images/size/w2000/2020/09/contact-1.jpg"
+        ></img>
         <p>
-          Follow us on social media to be notified of upcoming events and opportunities' or
-          something like that
+          CSESoc is the official representative body of computing students at UNSW. We are here to
+          fulfil the social, personal and professional needs of CSE students, and promote computing
+          through a variety of forms.
+        </p>
+        <h2>Contact</h2>
+        <ul>
+          <li>
+            <h3 className='text-2xl font-bold pt-5'>For general enquiries or feedback on our society</h3>
+            <p>
+              Please email{' '}
+              <a className="underline" href="mailto:info@csesoc.org.au">
+                info@csesoc.org.au
+              </a>
+            </p>
+          </li>
+          <li>
+            <h3 className='text-2xl font-bold pt-5'>For feedback on CSESoc Media</h3>
+            <p>
+              Please email{' '}
+              <a className="underline" href="mailto:media@csesoc.org.au">
+                media@csesoc.org.au
+              </a>
+            </p>
+          </li>
+          <li>
+            <h3 className='text-2xl font-bold pt-5'>For enquiries about sponsorship</h3>
+            <p>
+              Please email{' '}
+              <a className="underline" href="mailto:sponsorship@csesoc.org.au">
+                sponsorship@csesoc.org.au
+              </a>
+            </p>
+          </li>
+          <li>
+            <h3 className='text-2xl font-bold pt-5'>For sharing opportunities with our members</h3>
+            <p>
+              Please fill out{' '}
+              <a className="underline" href="https://forms.gle/7kk3RRBJbXo3Sip86">
+                this form
+              </a>
+            </p>
+          </li>
+        </ul>
+      </section>
+      <section className='py-5'>
+        <p className='text-xl'>
+          And be sure to follow us on social media to be notified of upcoming events and
+          opportunities!
         </p>
       </section>
       <Footer />
