3467cb33310fca0c5e0265994b4872e93139f260
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-06-27 02:57:30 +1000

    removed resources and contacts completely to remove redunancy (it is exactly the same component at ./resources

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Sponsors from '@/components/Sponsors/index';
 import About from '@/components/About';
 import Event from '@/components/Event';
 import ResourcesAndContacts from '@/components/ResourcesAndContacts';
+import ResourcesPage from './resources';
 
 export default function Home() {
   return (
@@ -10,7 +11,6 @@ export default function Home() {
       <Landing />
       <About />
       <Event />
-      <ResourcesAndContacts />
       <Sponsors />
     </section>
   );
