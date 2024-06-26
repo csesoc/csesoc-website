2c08ebbf59f9b6d9c7f47e9e7c73b98dd2bcc44e
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-06-27 01:41:51 +1000

    reduced iframe size and added padding to the bottom

--- frontend/src/pages/contact.tsx
+++ frontend/src/pages/contact.tsx
@@ -7,15 +7,17 @@ export default function ContactUs() {
     <section className="flex flex-col min-h-screen justify-between py-8 xl:px-24 md:px-10 px-5 relative overflow-hidden">
       <Navbar />
       <section>
-        <p className='text-xl pt-10'>
+        <p className="text-xl pt-10">
           CSESoc is the official representative body of computing students at UNSW. We are here to
           fulfil the social, personal and professional needs of CSE students, and promote computing
           through a variety of forms.
         </p>
         <ul>
           <li>
-            <h3 className='text-2xl font-bold pt-5'>For general enquiries or feedback on our society</h3>
-            <p className='text-xl'>
+            <h3 className="text-2xl font-bold pt-5">
+              For general enquiries or feedback on our society
+            </h3>
+            <p className="text-xl">
               Please email{' '}
               <a className="underline" href="mailto:info@csesoc.org.au">
                 info@csesoc.org.au
@@ -23,8 +25,8 @@ export default function ContactUs() {
             </p>
           </li>
           <li>
-            <h3 className='text-2xl font-bold pt-5'>For feedback on CSESoc Media</h3>
-            <p className='text-xl'>
+            <h3 className="text-2xl font-bold pt-5">For feedback on CSESoc Media</h3>
+            <p className="text-xl">
               Please email{' '}
               <a className="underline" href="mailto:media@csesoc.org.au">
                 media@csesoc.org.au
@@ -32,8 +34,8 @@ export default function ContactUs() {
             </p>
           </li>
           <li>
-            <h3 className='text-2xl font-bold pt-5'>For enquiries about sponsorship</h3>
-            <p className='text-xl'>
+            <h3 className="text-2xl font-bold pt-5">For enquiries about sponsorship</h3>
+            <p className="text-xl">
               Please email{' '}
               <a className="underline" href="mailto:sponsorship@csesoc.org.au">
                 sponsorship@csesoc.org.au
@@ -41,25 +43,30 @@ export default function ContactUs() {
             </p>
           </li>
           <li>
-            <h3 className='text-2xl font-bold pt-5'>For sharing opportunities with our members</h3>
-            <p className='text-xl'>
+            <h3 className="text-2xl font-bold pt-5">For sharing opportunities with our members</h3>
+            <p className="text-xl">
               Please fill out{' '}
               <a className="underline" href="https://forms.gle/7kk3RRBJbXo3Sip86">
                 this form
               </a>
             </p>
-            <iframe className='w-full h-[80vh]' src='https://forms.gle/7kk3RRBJbXo3Sip86'></iframe>
+            <iframe
+              className="w-1/2 h-[80vh] self-center"
+              src="https://forms.gle/7kk3RRBJbXo3Sip86"
+            ></iframe>
           </li>
         </ul>
       </section>
-      <section className='py-5'>
-        <p className='text-xl'>
+      <section className="py-5">
+        <p className="text-xl">
           And be sure to follow us on social media to be notified of upcoming events and
           opportunities!
         </p>
-    <Contacts/>
+        <div className='pb-40'>
+          <Contacts />
+        </div>
       </section>
-      
+
       <Footer />
     </section>
   );
