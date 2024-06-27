60de01877ed558cefa135faeb7f3dc27f474ed6a
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-06-27 19:31:10 +1000

    removed numbers from navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -26,19 +26,15 @@ const Navbar = () => {
       <div>
         <div className="md:flex xl:gap-36 lg:gap-20 md:gap-10 text-right font-bold hidden">
           <Link href="about">
-            <p className="text-[0.6rem] text-[#C4C5C8]">01</p>
             <div>{'//'} about us</div>
           </Link>
           <Link href="events">
-            <p className="text-[0.6rem] text-[#C4C5C8]">02</p>
             <div>{'//'} events</div>
           </Link>
           <Link href="resources">
-            <p className="text-[0.6rem] text-[#C4C5C8]">03</p>
             <div>{'//'} resources</div>
           </Link>
           <Link href="sponsors">
-            <p className="text-[0.6rem] text-[#C4C5C8]">04</p>
             <div>{'//'} sponsors</div>
           </Link>
         </div>
