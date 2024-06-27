e8e91bda2cc0ad4e8f52dc74c903e39ade5b5c11
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-06-27 22:15:10 +1000

    removing numbers

--- frontend/src/components/ResourcesAndContacts/index.tsx
+++ frontend/src/components/ResourcesAndContacts/index.tsx
@@ -15,7 +15,6 @@ const ResourcesAndContacts = () => {
           <Image src="assets/resources_bg.svg" alt="Background" className="-z-50 absolute" fill />
 
           <div className="text-center my-10">
-            <p className="text-[#3977F8] font-game text-xl">03</p>
             <h2 className="font-bold text-6xl">RESOURCES & CONTACTS</h2>
           </div>
 
