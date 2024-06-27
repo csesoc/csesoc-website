6ef6abea572caa881f82084a2efe5e07f9ae8372
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-06-27 19:39:49 +1000

    added button for events component

--- frontend/src/components/Event/index.tsx
+++ frontend/src/components/Event/index.tsx
@@ -6,10 +6,17 @@ const Event = () => {
         <h1 className="font-bold text-6xl">UPCOMING EVENTS</h1>
       </div>
       <div className="flex items-center">
+        <div className="flex flex-col">
         <p className="text-center text-3xl">
           We run a wide-variety of events for fun, learning new skills and careers. For full
           listings, check out the CSESoc Discord or our Facebook page!
         </p>
+        <a href="events">
+                <button className="mt-10 bg-white border text-[#3977F8] border-[#A7A6E5] text-lg rounded-xl w-[100%] xl:h-12 h-10">
+                  See all events
+                </button>
+              </a>
+        </div>
         <div className="bg-blue-500 w-4/5 h-96 ml-80">Placeholder</div>
       </div>
     </section>
