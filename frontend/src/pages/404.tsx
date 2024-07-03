dccb2dc1552c4275128700f9bf6ec1da0b3b3b22
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-07-04 01:49:39 +1000

    added more 404 page details

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -6,13 +6,15 @@ export default function Custom404() {
     <section className="flex flex-col min-h-screen justify-between py-8 xl:px-24 md:px-10 px-5 relative overflow-hidden">
       <div className="items-center text-center">
         <Navbar />
-        <h1 className="text-5xl font-bold pt-24">
+        <h1 className="text-5xl font-bold pt-24">404 - Page Not Found</h1>
+        <p className="text-4xl pt-10">
           Sorry! We can't find the page you're looking for!
-        </h1>
-        <p className="text-3xl pt-10">While we may have found some tomfoolery, 
-            <br/> <br/>
-            We haven't found your page :'(</p>
-        <a href='/'>
+          <br />
+        </p>
+        <p className="text-2xl pt-10">
+          While we may have found some tomfoolery, we haven't found your page :'(
+        </p>
+        <a href="/">
           <button className="mt-10 bg-white border text-[#3977F8] border-[#A7A6E5] text-lg rounded-xl w-[30%] xl:h-12 h-10">
             Back to saftey
           </button>
