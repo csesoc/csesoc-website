36db75e3ac686e639b95f09cc331dee0912c6315
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-07-04 01:30:56 +1000

    added Navbar + Footer + 404 page component

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export default function Custom404() {
+    return (
+        <>
+        <Navbar/>
+        <Footer/>
+        </>
+    )
+}
