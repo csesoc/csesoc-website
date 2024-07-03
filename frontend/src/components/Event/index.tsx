7ea1a2ed17a62ce18006b41ec4dde8cf8a876198
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-07-04 01:07:45 +1000

    made button smaller and allign to center

--- frontend/src/components/Event/index.tsx
+++ frontend/src/components/Event/index.tsx
@@ -5,13 +5,13 @@ const Event = () => {
         <h1 className="font-bold text-6xl">UPCOMING EVENTS</h1>
       </div>
       <div className="flex items-center">
-        <div className="flex flex-col">
+        <div className="flex flex-col text-center">
         <p className="text-center text-3xl">
           We run a wide-variety of events for fun, learning new skills and careers. For full
           listings, check out the CSESoc Discord or our Facebook page!
         </p>
         <a href="events">
-                <button className="mt-10 bg-white border text-[#3977F8] border-[#A7A6E5] text-lg rounded-xl w-[100%] xl:h-12 h-10">
+                <button className="mt-10 bg-white border text-[#3977F8] border-[#A7A6E5] text-lg rounded-xl w-[60%] xl:h-12 h-10">
                   See all events
                 </button>
               </a>
