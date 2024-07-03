b4b60de32ee9140190d6d06c82810f5199ce4a0c
QuadAces <107918856+QuadAces@users.noreply.github.com>
2024-07-04 01:17:37 +1000

    added AboutHomepage and removed pinned. Replaced About with AboutHomepage in index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,13 @@
 import Landing from '@/components/Landing';
 import Sponsors from '@/components/Sponsors/index';
-import About from '@/components/About';
 import Event from '@/components/Event';
+import AboutHomePage from '@/components/About/AboutHomepage';
 
 export default function Home() {
   return (
     <section>
       <Landing />
-      <About />
+      <AboutHomePage />
       <Event />
       <Sponsors />
     </section>
