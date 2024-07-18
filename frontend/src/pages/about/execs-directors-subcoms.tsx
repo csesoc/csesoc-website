31d76a0926715f3f441cbba1572bbd25282d93e2
Derek Xu <xuderek2004@gmail.com>
2024-07-18 18:46:38 +1000

    center execs and directors

--- frontend/src/pages/about/execs-directors-subcoms.tsx
+++ frontend/src/pages/about/execs-directors-subcoms.tsx
@@ -19,133 +19,136 @@ const ExecsDirectorsSubcommitteesPage = () => {
           <div className="flex justify-center items-center">
             <img src="/images/csesoc-team-2024.jpeg" alt="CSESoc Team 2024" width={700} height={700} />
           </div>
+        </div>
 
-          <div className="grid grid-cols-2 gap-6 grid-rows-auto">
-            <div className="my-5">
-              <h2 className="text-xl font-bold mb-2">Execs</h2>
-              <ul className="list-disc ml-6">
-                  <li>Elizabeth Zhu Chan - Co-President</li>
-                  <li>Lesley Lu - Co-President</li>
-                  <li>Ivan Chan - Secretary</li>
-                  <li>Justin Son - Treasurer</li>
-                  <li>Sophie Khov - Arc Delegate</li>
-                  <li>Catherine Kim - Grievance, Equity, Diversity and Inclusion Officer</li>
-                  <li>Amy Liu - Vice President (Internals)</li>
-                  <li>Nicole Jiang - Vice President (Externals)</li>
-                  <li>Matthew Lim - Vice President (Technicals)</li>
-              </ul>
+        <div className="my-5 text-center">
+            <h2 className="text-xl font-bold mb-2">Execs</h2>
+            <div className="flex justify-center">
+                <ul className="mx-4">
+                    <li>Elizabeth Zhu Chan - Co-President</li>
+                    <li>Lesley Lu - Co-President</li>
+                    <li>Ivan Chan - Secretary</li>
+                    <li>Justin Son - Treasurer</li>
+                    <li>Sophie Khov - Arc Delegate</li>
+                    <li>Catherine Kim - Grievance, Equity, Diversity and Inclusion Officer</li>
+                    <li>Amy Liu - Vice President (Internals)</li>
+                    <li>Nicole Jiang - Vice President (Externals)</li>
+                    <li>Matthew Lim - Vice President (Technicals)</li>
+                </ul>
             </div>
+        </div>
 
-            <div className="my-5">
+        <div className="grid grid-cols-2 gap-6">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Careers</h2>
-                <ul className="list-disc ml-6">
+                <ul className="space-y-1">
                     <li>Eric Kang</li>
                     <li>Jasmine Xian</li>
                     <li>Max Lee</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Competitions</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Aliff Azlan</li>
                     <li>Haibing Wang</li>
                     <li>Diego Untalan</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Creative</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Anna Wang</li>
                     <li>Jordan Djamaan</li>
                     <li>Shabinda Sarkaria</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Education</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Alina Jin</li>
                     <li>JJ Roberts-White</li>
                     <li>Liam Smith</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Events</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Emma Nguyen</li>
                     <li>Jalaj Jain</li>
                     <li>Jenny Lee</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">HR</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Aryan Chauhan</li>
                     <li>Edwin Tang</li>
                     <li>Jasmine Guan</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">IT</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Derek Xu</li>
                     <li>Dominic Cheung</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Marketing</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Aaron Lee</li>
                     <li>Lisa Lin</li>
                     <li>Zitian Qin</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Media</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Dylan Hu</li>
                     <li>Gloria Xi</li>
                     <li>Leo Ng Maisnam</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Online</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Amy Tian</li>
                     <li>Brandon Tan</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Outreach</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Joyce He</li>
                     <li>Sapphire Wildie</li>
                 </ul>
             </div>
 
-            <div className="my-5">
+            <div className="my-5 flex flex-col justify-center items-center text-center">
                 <h2 className="text-xl font-bold mb-2">Socials</h2>
-                <ul className="list-disc ml-6">
+                <ul>
                     <li>Davis Lim</li>
                     <li>Jennifer Yu</li>
                     <li>Susie Xia</li>
                 </ul>
             </div>
-          </div>
 
-          <p className="text-xl bold my-5">
-             Get to know them better <a href="https://media.csesoc.org.au/fyg24-meet-csesoc/" className="text-blue-500 hover:underline">here</a>!
+            <p className="text-xl bold my-5">
+              Get to know them better <a href="https://media.csesoc.org.au/fyg24-meet-csesoc/" className="text-blue-500 hover:underline">here</a>!
             </p>
         </div>
+
 
         <div className="border-t border-gray-300 my-5"></div>
 
