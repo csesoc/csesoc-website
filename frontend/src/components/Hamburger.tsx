a2dbfd44cd657ac78530c274e48855ee98f0be94
Eric <eric.wu3@student.unsw.edu.au>
2024-08-01 16:37:16 +1000

    Making hamburger link open in seperate tab

--- frontend/src/components/Hamburger.tsx
+++ frontend/src/components/Hamburger.tsx
@@ -51,7 +51,7 @@ export default function Hamburger() {
                 <Link href={'./contact-us'}>Contact Us</Link>
               </li>
               <li className="py-2 text-lg">
-                <a href="https://csesoc-merch.square.site/">Merch Store</a>
+                <a target="_blank" href="https://csesoc-merch.square.site/">Merch Store</a>
               </li>
             </ul>
           </motion.div>
