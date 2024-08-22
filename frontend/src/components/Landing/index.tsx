ff29724b4bc393c98eb043cbc85c678dddd1138e
Wolfdragon24 <dragonbomber24@gmail.com>
2024-08-22 20:53:23 +1000

    Append left-in underline animation with link to UNSW School of CSE

--- frontend/src/components/Landing/index.tsx
+++ frontend/src/components/Landing/index.tsx
@@ -20,7 +20,13 @@ const Landing = () => {
             <p>{'<h2>'}</p>
             <h2 className="font-bold 2xl:text-3xl text-xl">
               We are the principal representative body for{' '}
-              <span className="text-[#3977F9]">UNSW computing</span> students.
+              <a href="https://www.unsw.edu.au/engineering/our-schools/computer-science-and-engineering"
+                 target = "_blank"
+                 className="text-[#3977F9] relative after:bg-current after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:duration-200"
+              >
+                UNSW computing
+              </a>
+              {' '}students.
             </h2>
             <p>{'</h2>'}</p>
           </div>
