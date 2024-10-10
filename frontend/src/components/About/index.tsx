4ea1a11b304801ee6f8fe8c1bf3eda4e24e9a897
Saucy_Salad <andrewbruce222@gmail.com>
2024-10-11 03:38:22 +1100

    landing page about section changes

--- frontend/src/components/About/index.tsx
+++ frontend/src/components/About/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Stats from '../Stats';
+import Github from '../Github';
 
 const About = () => {
   return (
@@ -13,47 +13,10 @@ const About = () => {
         <div className="lg:grid grid-cols-2 flex-1 max-w-[90rem]">
           {/* LEFT SIDE */}
           <div className="col-span-2">
-{/*            <div className="flex items-center justify-center">
-              <Image src="/assets/csesoc_icon.svg" alt="CSESoc Icon" width={150} height={150} />
-            </div>*/}
-{/*            <h2 className="mt-10 text-3xl font-extrabold">CSESoc</h2>
-            <p className="text-[#727B8C] font-medium">unsw-computer-science-engineering-society</p>*/}
-            <Stats/>
-{/*            <button className="bg-[#444F6F] w-full my-5 py-2 rounded">Follow</button>*/}
-{/*            <p>
-              CSESoc is the principal representative body for UNSW computing students.
-            </p>
-            <div className="my-4 flex gap-5">
-              <Image src="/assets/people_icon.svg" alt="People" width={20} height={20} />
-              <div>
-                432 <span className="text-[#727B8C]">members</span> · 342{' '}
-                <span className="text-[#727B8C]">subcom</span>
-              </div>
-            </div>
-            <div className="my-4 flex gap-5">
-              <Image src="/assets/location_icon.svg" alt="Location" width={20} height={20} />
-              Sydney, Australia
-            </div>
-            <div className="flex gap-5">
-              <Image src="/assets/mail_icon.svg" alt="Mail" width={20} height={20} />
-              info@csesoc.org.au
-            </div>*/}
+            <Github/>
           </div>
           {/* RIGHT SIDE */}
           <div className="col-span-4 lg:mt-0 mt-10 pt-8">
-{/*            <div className="rounded border border-[#595F6D] p-5 h-auto">
-              <p className="text-xs">
-                csesoc/README<span className="text-[#7A8192]">.md</span>
-              </p>
-              <p className="mt-5">
-                CSESoc is one of the biggest and most active societies at UNSW, catering to over 3500 CSE
-                students spanning across degrees in Computer Science, Software Engineering,
-                Bioinformatics and Computer Engineering. 
-                <br /><br />
-                We are here to fulfil the social, personal and 
-                professional needs of CSE students, and promote computing through a variety of forms.
-              </p>
-            </div>*/}
             <div className="mt-10 mb-32">
               Pinned
               {/* TODO: refactor all of this */}
