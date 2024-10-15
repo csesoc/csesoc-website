f277d05cb128edd4e47969c874448b189f5b8cfe
Ming Xuan Yong <z5439537@unsw.edu.au>
2024-10-15 13:13:22 +1100

    Edited events page such that it can be visible when events is clicked

--- frontend/src/pages/events.tsx
+++ frontend/src/pages/events.tsx
@@ -1,21 +1,13 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
-import axios from 'axios';
+// import axios from 'axios';
+import Events from '@/components/Event';
 
 export default function EventsPage() {
-  const pingBackend = async () => {
-    axios
-      .get(
-        `http://${process.env.NEXT_PUBLIC_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/ping`
-      )
-      .then((resp) => console.log(resp.data))
-      .catch((error) => console.log(error.message));
-  };
-
   return (
     <section className="flex flex-col min-h-screen justify-between py-8 xl:px-24 md:px-10 px-5 relative overflow-hidden">
       <Navbar />
-      <button onClick={() => pingBackend()}>Ping backend</button>
+      <Events/>
       <Footer />
     </section>
   );
