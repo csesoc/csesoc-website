6f5018a654bb3a819d012d4db378224d990852ae
Wolfdragon24 <dragonbomber24@gmail.com>
2024-10-18 21:24:29 +1100

    Replace gallery library

--- frontend/src/components/Event/EventsGallery.tsx
+++ frontend/src/components/Event/EventsGallery.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import { previousEvents } from '../../../public/data/events';
-import { Gallery } from "react-grid-gallery";
+import { RowsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/rows.css";
 
 const EventGallery = () => {
   const formattedEvents = previousEvents.map((event) => {
     return {
       src: event.image,
-      height: 256,
-      width: 400,
-      customOverlay: (
-        <div className="absolute inset-0 bg-black bg-opacity-60 opacity-100 transition-opacity duration-300 flex items-center justify-center">
-          <div className="text-white text-center p-4">
-            <h2 className="font-extrabold text-xl mb-2">{event.title}</h2>
-            <h3 className="font-bold text-lg">{event.location}</h3>
-            <p className="mt-2">
-              {formatEventDate(event.startTime, event.endTime)}
-            </p>
-          </div>
-        </div>
-      ),
+      height: 80,
+      width: 120,
     };
   });
 
   return (
-    <Gallery
-      images={formattedEvents}
-      enableImageSelection={false}
+    <RowsPhotoAlbum
+      photos={formattedEvents}
+      render={{
+        extras: (_, { index }) => (
+          <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+          <div className="text-white text-center p-4">
+            <h2 className="font-extrabold text-xl mb-2">{previousEvents[index].title}</h2>
+            <h3 className="font-bold text-lg">{previousEvents[index].location}</h3>
+            <p className="mt-2">
+              {formatEventDate(previousEvents[index].startTime, previousEvents[index].endTime)}
+            </p>
+          </div>
+        </div>
+        ),
+      }}
     />
   );
 };
