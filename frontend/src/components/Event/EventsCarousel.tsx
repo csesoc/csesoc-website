91a1b1013ae944c7e4779930518751c969084570
Wolfdragon24 <dragonbomber24@gmail.com>
2024-10-18 20:52:35 +1100

    Formatting fix

--- frontend/src/components/Event/EventsCarousel.tsx
+++ frontend/src/components/Event/EventsCarousel.tsx
@@ -36,22 +36,22 @@ export default function EventsCarousel() {
     >
       {events.map((event, index) => (
         <div key={index} className="w-full text-center">
-        <a href={event.link} target="_blank" rel="noopener noreferrer" className="relative block w-full h-64 group transition-opacity duration-3000">
-          <img
-            src={event.image}
-            alt={event.title}
-            className="w-full h-64 object-contain rounded-md"
-          />
-          <div className="w-full h-64 absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center rounded-md">
-            <div className="text-white text-center p-4">
-              <h2 className="font-extrabold text-xl mb-2">{event.title}</h2>
-              <h3 className="font-bold text-lg">{event.location}</h3>
-              <p className="mt-2">
-                {formatEventDate(event.startTime, event.endTime)}
-              </p>
+          <a href={event.link} target="_blank" rel="noopener noreferrer" className="relative block w-full h-64 group transition-opacity duration-3000">
+            <img
+              src={event.image}
+              alt={event.title}
+              className="w-full h-64 object-contain rounded-md"
+            />
+            <div className="w-full h-64 absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center rounded-md">
+              <div className="text-white text-center p-4">
+                <h2 className="font-extrabold text-xl mb-2">{event.title}</h2>
+                <h3 className="font-bold text-lg">{event.location}</h3>
+                <p className="mt-2">
+                  {formatEventDate(event.startTime, event.endTime)}
+                </p>
+              </div>
             </div>
-          </div>
-        </a>
+          </a>
         </div>
       ))}
     </Carousel>
