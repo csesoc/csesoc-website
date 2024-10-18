8e189b3db410308173ed9a5ed0aef0267a18dbda
Derek Xu <xuderek2004@gmail.com>
2024-10-18 16:57:47 +1100

    update sponsor info

--- frontend/public/data/sponsorInfos.ts
+++ frontend/public/data/sponsorInfos.ts
@@ -9,16 +9,16 @@ export const diamondLinks: sponsorInfo[] = [
   {
     href: 'https://www.janestreet.com/',
     svg: '/assets/janestreet_logo.svg',
-    alt: 'Janestreet logo',
+    alt: 'Jane Street logo',
     description:
-      'Jane Street is a research-driven trading firm where curious people work together on deep problems'
+      'Jane Street is a research-driven trading firm where curious people work together on deep problems.'
   },
   {
-    href: 'https://www.tiktok.com/en/',
+    href: 'https://careers.tiktok.com/',
     svg: '/assets/tiktok_logo.svg',
     alt: 'Tiktok logo',
     description:
-      'TikTok is a social media platform for creating, sharing and discovering short videos'
+      'TikTok is a social media platform for creating, sharing and discovering short videos.'
   }
 ];
 
@@ -28,14 +28,14 @@ export const goldLinks: sponsorInfo[] = [
     svg: '/assets/atlassian_logo.svg',
     alt: 'Atlassian logo',
     description:
-      'Atlassian Corporation is an Australian-American software company that develops products for software developers, and project managers among other groups'
+      'Atlassian Corporation is an Australian-American software company that develops products for software developers, and project managers among other groups.'
   },
   {
     href: 'https://www.citadel.com/',
     svg: '/assets/citadel_logo.svg',
     alt: 'Citadel logo',
     description:
-      "We are an alternative investment manager working on behalf of the world's preeminent institutions. Discover our work, teams, careers and more"
+      "We are an alternative investment manager working on behalf of the world's preeminent institutions. Discover our work, teams, careers and more."
   },
   {
     href: 'https://www.imc.com/ap/',
@@ -49,21 +49,21 @@ export const goldLinks: sponsorInfo[] = [
     svg: '/assets/neara_logo.svg',
     alt: 'Neara logo',
     description:
-      'Neara electric utility software is a physics-enabled platform that builds 3D interactive models of critical infrastructure networks and /assets.'
+      'Neara electric utility software is a physics-enabled platform that builds 3D interactive models of critical infrastructure networks and assets.'
   },
   {
     href: 'https://safetyculture.com/',
     svg: '/assets/safetyculture_logo.svg',
     alt: 'SafetyCulture logo',
     description:
-      'Get to the root cause of workplace trends with total visibility across your organization. Use data from completed inspections, reported incidents, sensors, and asset history to keep workers safe, and prevent things from happening in the first place'
+      'Get to the root cause of workplace trends with total visibility across your organization. Use data from completed inspections, reported incidents, sensors, and asset history to keep workers safe, and prevent things from happening in the first place.'
   },
   {
     href: 'https://www.zurich.com.au/',
     svg: '/assets/zurich_logo.svg',
     alt: 'Zurich logo',
     description:
-      "Zurich is one of the world's most experienced insurers whose strategy focuses on providing the right general insurance and life insurance solutions for its clients"
+      "Zurich is one of the world's most experienced insurers whose strategy focuses on providing the right general insurance and life insurance solutions for its clients."
   }
 ];
 
@@ -73,41 +73,41 @@ export const silverLinks: sponsorInfo[] = [
     svg: '/assets/appian_logo.svg',
     alt: 'Appian logo',
     description:
-      'Appian Corporation is an American cloud computing and enterprise software company headquartered in McLean, Virginia, part of the Dulles Technology Corridor. The company sells a platform as a service for building enterprise software applications'
+      'Appian Corporation is an American cloud computing and enterprise software company headquartered in McLean, Virginia, part of the Dulles Technology Corridor. The company sells a platform as a service for building enterprise software applications.'
   },
   {
     href: 'https://www.flowtraders.com/',
     svg: '/assets/flowtraders_logo.svg',
     alt: 'FlowTraders logo',
     description:
-      'Flow Traders is a proprietary trading firm. A market maker, it provides liquidity in the securities market by using high frequency and quantitative trading strategies'
+      'Flow Traders is a proprietary trading firm. A market maker, it provides liquidity in the securities market by using high frequency and quantitative trading strategies.'
   },
   {
     href: 'https://www.jds.net.au/',
     svg: '/assets/jds_logo.svg',
     alt: 'jds logo',
-    description: "JDS Australia delivers specialist services in a set of technologies and capabilities that ensure critical IT systems work",
+    description: "JDS Australia delivers specialist services in a set of technologies and capabilities that ensure critical IT systems work.",
   },
   {
     href: 'https://www.macquarie.com.au/',
     svg: '/assets/macquarie_logo.svg',
     alt: 'Macquarie logo',
     description:
-      'Macquarie Bank offers transaction accounts, home loans, credit cards, online banking, business banking and more'
+      'Macquarie Bank offers transaction accounts, home loans, credit cards, online banking, business banking and more.'
   },
   {
     href: 'https://optiver.com/',
     svg: '/assets/optiver_logo.svg',
     alt: 'Optiver logo',
     description:
-      'Optiver is a global market maker. As one of the oldest market making firms in the world, Optiver has been improving financial markets since 1986'
+      'Optiver is a global market maker. As one of the oldest market making firms in the world, Optiver has been improving financial markets since 1986.'
   },
   {
     href: 'https://quantium.com/',
     svg: '/assets/quantium_logo.svg',
     alt: 'Quantium logo',
     description:
-      "Quantium has developed a world-class data science and AI solution that has transformed the accuracy of Walmart's prediction of customers' needs at scale"
+      "We combine the best of human and artificial intelligence to power possibilities for individuals, organisations and society. Through the application of world leading, advanced analytics solutions, we help our clients solve their most complex problems."
   },
   {
     href: 'https://quickli.com.au/',
@@ -119,15 +119,15 @@ export const silverLinks: sponsorInfo[] = [
   {
     href: 'https://www.revolutionise.com.au/',
     svg: '/assets/revsport_logo.svg',
-    alt: 'RevolutioniseSport logo',
+    alt: 'revolutioniseSport logo',
     description:
-      'revolutioniseSPORT is an online management platform for sports of all shapes and sizes'
+      'revolutioniseSPORT is an online management platform for sports of all shapes and sizes.'
   },
   {
     href: 'https://www.recordpoint.com/',
     svg: '/assets/recordpoint_logo.svg',
     alt: 'RecordPoint logo',
-    description: 'Manage all your data and records in one central place – without moving them'
+    description: 'Manage all your data and records in one central place – without moving them.'
   },
   {
     href: 'https://sig.com/',
@@ -141,7 +141,7 @@ export const silverLinks: sponsorInfo[] = [
     svg: '/assets/zip_logo.svg',
     alt: 'Zip logo',
     description:
-      "Zip Co Limited is a global 'buy now pay later' financial technology company with operations in Australia, New Zealand and the USA"
+      "Zip Co Limited is a global 'buy now pay later' financial technology company with operations in Australia, New Zealand and the USA."
   },
   {
     href: 'https://www.canva.com/en_au/',
