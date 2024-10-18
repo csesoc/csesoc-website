8392d559197919f879223c16c53fed56372b8a4c
Derek Xu <xuderek2004@gmail.com>
2024-10-18 22:54:53 +1100

    trial boxes

--- frontend/src/components/Event/EventsBrief.tsx
+++ frontend/src/components/Event/EventsBrief.tsx
@@ -4,24 +4,50 @@ import EventsCarousel from './EventsCarousel';
 
 const EventBrief = () => {
   return (
+    // <section className="py-8 xl:px-24 sm:px-10 px-5" id="events">
+    //   <div className="text-center my-10">
+    //     <h1 className="font-bold text-6xl">UPCOMING EVENTS</h1>
+    //   </div>
+    //   {events.length !== 0 ?
+    //     <EventsCarousel/> :
+    //     <div className="flex items-center justify-center h-96">
+    //       <p className="text-4xl">No upcoming events... check back here later!</p>
+    //     </div>
+    //   }
+    //   <div className="flex justify-center items-center">
+    //     <a href="events">
+    //       <button className="mt-10 bg-white border text-[#3977F8] border-[#A7A6E5] text-lg rounded-xl w-[20rem] xl:h-12 h-10 hover-animate">
+    //         See all events
+    //       </button>
+    //     </a>
+    //   </div>
+    // </section>
     <section className="py-8 xl:px-24 sm:px-10 px-5" id="events">
-      <div className="text-center my-10">
-        <h1 className="font-bold text-6xl">UPCOMING EVENTS</h1>
-      </div>
-      {events.length !== 0 ?
-        <EventsCarousel/> :
-        <div className="flex items-center justify-center h-96">
-          <p className="text-4xl">No upcoming events... check back here later!</p>
-        </div>
-      }
       <div className="flex justify-center items-center">
-        <a href="events">
-          <button className="mt-10 bg-white border text-[#3977F8] border-[#A7A6E5] text-lg rounded-xl w-[20rem] xl:h-12 h-10 hover-animate">
-            See all events
-          </button>
-        </a>
+        <div
+          className="rounded-[1rem] border-2 border-[#595F6D] h-auto overflow-hidden w-full max-w-[90rem] flex flex-col gap-8 py-8"
+        >
+          <div className="text-center my-10">
+            <h1 className="font-bold text-6xl">UPCOMING EVENTS</h1>
+          </div>
+          {events.length !== 0 ? (
+            <EventsCarousel />
+          ) : (
+            <div className="flex items-center justify-center h-96">
+              <p className="text-4xl">No upcoming events... check back here later!</p>
+            </div>
+          )}
+          <div className="flex justify-center items-center">
+            <a href="events">
+              <button className="mt-10 bg-white border text-[#3977F8] border-[#A7A6E5] text-lg rounded-xl w-[20rem] xl:h-12 h-10 hover-animate">
+                See all events
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
     </section>
+
   );
 };
 
--- frontend/src/components/Sponsors/index.tsx
+++ frontend/src/components/Sponsors/index.tsx
@@ -4,17 +4,45 @@ import SponsorCarousel from './SponsorCarousel';
 
 const Sponsors = () => {
   return (
+    // <section
+    //   className="flex flex-col min-h-screen py-8 xl:px-24 sm:px-10 px-8 relative mt-20"
+    //   id="sponsors"
+    // >
+    //   <div className="text-center">
+    //     <h2 className="font-bold text-6xl">OUR SPONSORS</h2>
+    //   </div>
+    //   <div className="flex-1 flex justify-center items-center my-15">
+    //     <div
+    //       className="rounded border border-[#595F6D] h-auto overflow-hidden w-[90rem] flex flex-col gap-8 py-8"
+    //     >
+    //       <SponsorCarousel />
+    //       <div className="flex justify-center items-center">
+    //         <a href="sponsors">
+    //           <button className="bg-white border text-[#3977F8] border-[#A7A6E5] text-lg rounded-xl w-[20rem] xl:h-12 h-10 hover-animate">
+    //             See all sponsors
+    //           </button>
+    //         </a>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <Footer />
+    //   <img
+    //     src="assets/sponsors_backdrop.svg"
+    //     alt="Sponsors backdrop"
+    //     className="absolute bottom-0 left-0 w-screen -z-10"
+    //   />
+    // </section>
     <section
       className="flex flex-col min-h-screen py-8 xl:px-24 sm:px-10 px-8 relative mt-20"
       id="sponsors"
     >
-      <div className="text-center">
-        <h2 className="font-bold text-6xl">OUR SPONSORS</h2>
-      </div>
-      <div className="flex-1 flex justify-center items-center my-15">
+      <div className="flex-1 flex flex-col justify-center items-center">
         <div
-          className="rounded border border-[#595F6D] h-auto overflow-hidden w-[90rem] flex flex-col gap-8 py-8"
+          className="rounded-[1rem] border-2 border-[#595F6D] h-auto overflow-hidden w-full max-w-[90rem] flex flex-col gap-8 py-16"
         >
+          <div className="text-center">
+            <h2 className="font-bold text-6xl">OUR SPONSORS</h2>
+          </div>
           <SponsorCarousel />
           <div className="flex justify-center items-center">
             <a href="sponsors">
@@ -25,7 +53,9 @@ const Sponsors = () => {
           </div>
         </div>
       </div>
+      
       <Footer />
+      
       <img
         src="assets/sponsors_backdrop.svg"
         alt="Sponsors backdrop"
