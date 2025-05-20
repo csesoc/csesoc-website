2eaf8c80c82132b1ae08ca9765142adb4a7bb615
louis <louislim2005@gmail.com>
2025-05-20 13:30:42 +0800

    navbar is now consistent across all pages

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,10 +4,14 @@ import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="flex flex-col min-h-screen justify-between py-8 xl:px-24 md:px-10 px-5 relative">
+    <section className="flex flex-col min-h-screen justify-between py-8 relative">
       <Navbar />
-      {children}
-      <Footer />
+      
+      <div className='xl:px-24 md:px-10 px-5 '>
+        {children}
+        <Footer />
+      </div>
+      
     </section>
   );
 };
