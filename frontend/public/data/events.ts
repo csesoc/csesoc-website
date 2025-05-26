0a7c743ca718dab70433984e7778165b0de475a7
dylan <robouser51@gmail.com>
2025-05-26 14:01:42 +1000

    update events

--- frontend/public/data/events.ts
+++ frontend/public/data/events.ts
@@ -1,4 +1,3 @@
-
 export type eventInfo = {
   title: string;
   startTime: string;
@@ -10,6 +9,22 @@ export type eventInfo = {
 };
 
 export const events: eventInfo[] = [
+  {
+    title: "Brawl Stars Tournament",
+    startTime: "Thursday, 29 May 2025 20:00:00",
+    endTime: "Thursday, 29 May 2025 22:30:00",
+    location: "Online via Discord",
+    image: "/images/events/brawl_stars.jpg",
+    link: "https://www.facebook.com/events/1251081513377441"
+  },
+  {
+    title: "Rookie Code Rumble",
+    startTime: "Friday, 30 May 2025 18:00:00",
+    endTime: "Friday, 6 June 2025 23:59:59",
+    location: "Online (kick-off location TBD)",
+    image: "/images/events/rookie_code_rumble.jpg",
+    link: "https://www.facebook.com/events/1062484559164505"
+  }
 ];
 
 export const previousEvents: eventInfo[] = [
@@ -18,7 +33,7 @@ export const previousEvents: eventInfo[] = [
     startTime: "Wednesday, 23 Apr 2025 13:00:00",
     endTime: "Wednesday, 23 Apr 2025 15:00:00",
     location: "The Quad, UNSW",
-    image: "https://scontent.fsyd3-2.fna.fbcdn.net/v/t39.30808-6/496060586_1215569570352652_7618135313648050813_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=108&ccb=1-7&_nc_sid=75d36f&_nc_ohc=wxKd3v8uu-UQ7kNvwF7VUJH&_nc_oc=Adn1GeV1aYTvBJK9DCFeyIvU1_LMpXs8dVq3sLyzudpnO7tC_5SAjQ0wg3p1Inj8fyU&_nc_zt=23&_nc_ht=scontent.fsyd3-2.fna&_nc_gid=9cGTNTyPVfP2wfnB-gSRnQ&oh=00_AfLs1T4Dci7K25flrhE5CKdxV5APp1Uep4yMPfb5eOGmDQ&oe=682F9C19",
+    image: "/images/events/flower_exchange.jpg",
     link: "https://www.facebook.com/events/623535984011063"
   },
   {
@@ -26,7 +41,7 @@ export const previousEvents: eventInfo[] = [
     startTime: "Friday, 28 Mar 2025 19:00:00",
     endTime: "Friday, 28 Mar 2025 23:30:00",
     location: "Meet at Hyde Park",
-    image: "https://scontent.fsyd3-1.fna.fbcdn.net/v/t39.30808-6/491528916_1195030505739892_8262414584987128619_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=107&ccb=1-7&_nc_sid=75d36f&_nc_ohc=Ig-pYWLBNBoQ7kNvwHhQxqU&_nc_oc=Adm5zSvjjwjabfe6TWgfpkTaO9HtoVl5sq2uC6bqJkSocn5cIVTFBI0PAI1JFSPMd10&_nc_zt=23&_nc_ht=scontent.fsyd3-1.fna&_nc_gid=0e81miaVYxUQD5M2EKuuZQ&oh=00_AfJpq6jA7xm6V-OBGe9-rk4v8nOGvVanBNByphDracG3zQ&oe=682F8D65",
+    image: "/images/events/all_the_stars.jpg",
     link: "https://www.facebook.com/events/1171496917957222"
   },
   {
@@ -34,7 +49,7 @@ export const previousEvents: eventInfo[] = [
     startTime: "Sunday, 13 Apr 2025 13:00:00",
     endTime: "Sunday, 13 Apr 2025 16:00:00",
     location: "UNSW Village Green (Multi-purpose caged courts near Sam Cracknell Pavilion)",
-    image: "https://scontent.fsyd3-1.fna.fbcdn.net/v/t39.30808-6/492563472_1201711188405157_1130478402657046812_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=104&ccb=1-7&_nc_sid=75d36f&_nc_ohc=7S7A1l8ev04Q7kNvwH7Srql&_nc_oc=AdmdnTKGrmKojSVdvdWpPSmiDsEo44glHV5XtF4j1htLv_uYXjm2WZ0waSfTSdkn36Y&_nc_zt=23&_nc_ht=scontent.fsyd3-1.fna&_nc_gid=oXshtlGgXg9DOzupr9IGxw&oh=00_AfICeCWN_hyBG0yGkg7sQWOj2H6zXd2wbm0G8ZwEA544IQ&oe=682F89D5",
+    image: "/images/events/wheelchair_basketball.jpg",
     link: "https://www.facebook.com/events/3852672894949394"
   }
 ];
