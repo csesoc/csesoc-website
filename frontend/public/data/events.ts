5ac79bb002e7d70db4d8b150b8c7743db634d293
dlyn <100419289+dlynz@users.noreply.github.com>
2025-06-03 12:23:45 +1000

    [I2-13] Completely updated website data for 2025 (#48)
    * completely update website data
    
    * update events
    
    * Update csesoc team picture and append platforms portfolio
    
    * Formatting adjustment
    
    ---------
    
    Co-authored-by: Wolfdragon24 <dragonbomber24@gmail.com>

--- frontend/public/data/events.ts
+++ frontend/public/data/events.ts
@@ -1,97 +1,55 @@
-
 export type eventInfo = {
   title: string;
   startTime: string;
   endTime: string;
   location: string;
-  description: string;
+  // description: string;
   image: string;
   link: string;
 };
 
 export const events: eventInfo[] = [
   {
-    title: 'CSESoc Hoodie General Release 2024',
-    startTime: 'Monday, 7 Oct 2024 12:00:00',
-    endTime: 'Friday, 18 Oct 2024 23:59:59',
-    location: 'Online',
-    description: 'After much anticipated wait 🤩, from your design submissions and personal input, our 2024 CSESoc Shell hoodies are now available for order! 🥳',
-    image: 'https://scontent.fsyd11-2.fna.fbcdn.net/v/t39.30808-6/462221120_1045429977366613_3924200758993686299_n.jpg?stp=dst-jpg_s960x960&_nc_cat=107&ccb=1-7&_nc_sid=75d36f&_nc_ohc=tt-N_Bd5fd4Q7kNvgFdY5yk&_nc_zt=23&_nc_ht=scontent.fsyd11-2.fna&_nc_gid=AG_QiCaGBWyXh0lwZdZZ73k&oh=00_AYD8zSJw_LFOHaIG7oOFtws9uIbXrCyRwIS0IZkh6ipInQ&oe=67182124',
-    link: 'https://www.facebook.com/events/858016553106187',
+    title: "Brawl Stars Tournament",
+    startTime: "Thursday, 29 May 2025 20:00:00",
+    endTime: "Thursday, 29 May 2025 22:30:00",
+    location: "Online via Discord",
+    image: "/images/events/brawl_stars.jpg",
+    link: "https://www.facebook.com/events/1251081513377441"
   },
   {
-    title: 'HUMANS OF CSE OCTOBER ARTICLE - TRANSFERRING DEGREES',
-    startTime: 'Monday, 14 Oct 2024, 20:00:00',
-    endTime: 'Monday, 21 Oct 2024, 23:00:00',
-    location: 'Online',
-    description: 'Come along with us in this month\'s article 🌎, as we follow the journey of CSE students who made the switch to these degrees! 🔄 We explore how they decided to transfer 📜 , what challenges there were getting started, and all the exciting things they\'re up to now! 🚀',
-    image: 'https://scontent.fsyd11-2.fna.fbcdn.net/v/t39.30808-6/463321890_1051317113444566_587573735370794997_n.jpg?stp=dst-jpg_s960x960&_nc_cat=103&ccb=1-7&_nc_sid=75d36f&_nc_ohc=5o-MEkKeoecQ7kNvgEVDfXP&_nc_zt=23&_nc_ht=scontent.fsyd11-2.fna&_nc_gid=Ama2uSCPexSL8fx4vEvJSzh&oh=00_AYAwBP-EHTZUai43wHVQQZ7JRVGuZ4RdOih_C9ocLR1tqw&oe=67182C1D',
-    link: 'https://www.facebook.com/events/544877388231818/'
-  },
-  {
-    title: 'Meet the Arista Warriors - BBQ And Talk',
-    startTime: 'Wednesday, 23 Oct 2024, 12:00:00',
-    endTime: 'Wednesday, 23 Oct 2024, 16:00:00',
-    location: 'John Lions Garden, UNSW',
-    description: "Ready to meet the creators of the fastest switches in the world? ⚡ Join us for an interactive talk with the Arista Warriors! 👥 Get a behind-the-scenes look at their network switches 🖥️, learn how their advanced software drives the performance 💻, and meet the talented teams that make it all happen 👨‍💻!",
-    image: 'https://scontent.fsyd11-1.fna.fbcdn.net/v/t39.30808-6/463219819_1051611690081775_3133232766646231661_n.jpg?stp=dst-jpg_s960x960&_nc_cat=110&ccb=1-7&_nc_sid=75d36f&_nc_ohc=DmRNYX-_AtQQ7kNvgFnwOCH&_nc_zt=23&_nc_ht=scontent.fsyd11-1.fna&_nc_gid=AqkzDMqF36HWZbR7hgWA7AK&oh=00_AYDepVjHbmg5wNZvnIh92P49ESA1i-MyUSBA7G09lSQd-g&oe=67181093',
-    link: 'https://www.facebook.com/events/1739162536859227/'
-  },
-  {
-    title: 'Nostalgia Night',
-    startTime: 'Wednesday, 23 Oct 2024, 17:00:00',
-    endTime: 'Wednesday, 23 Oct 2024, 19:00:00',
-    location: 'Design Next Studio, Level 5, J17, UNSW',
-    description: 'Feeling trapped in the endless cycle of assignments and uni commitments? 📚😓 Longing for simpler times when your biggest worry was whether you should watch Giggle and Hoot or make loom bands with your sibling? 📺🍭 Well, dust off your Pokémon cards and polish those yo-yos, because CSESoc is about to send you back to the past with NOSTALGIA NIGHT! 🚀⏰',
-    image: 'https://scontent.fsyd11-1.fna.fbcdn.net/v/t39.30808-6/462701368_1048050000437944_2801863201980347260_n.jpg?stp=dst-jpg_s960x960&_nc_cat=104&ccb=1-7&_nc_sid=75d36f&_nc_ohc=m4Sy0L-V9i8Q7kNvgGYoF5X&_nc_zt=23&_nc_ht=scontent.fsyd11-1.fna&_nc_gid=A2nTAu82YR1NE7-13Zxaw3n&oh=00_AYAk4v2MHtB9pFASHYQe2wnY153oxVDZ8qaI_my6JMPL0Q&oe=671828EA',
-    link: 'https://www.facebook.com/events/1244647416584933/',
-  },
-  {
-    title: 'CSESoc x EngSoc: Hallo-Wasted',
-    startTime: 'Friday, 1 Nov 2024, 19:00:00',
-    endTime: 'Friday, 1 Nov 2024, 23:00:00',
-    location: 'Scruffy Murphy\'s',
-    description: 'Fear no more — or even more 🫣, as CSESoc and EngSoc invites you to crawl out 🐺🕷️ from your study dungeons for the most nerve-wrecking night you won’t forget — at the Hallo-Wasted Party! 🍾🙀',
-    image: 'https://scontent.fsyd11-1.fna.fbcdn.net/v/t39.30808-6/462393945_1046485953927682_3594406987464705203_n.jpg?stp=dst-jpg_s960x960&_nc_cat=104&ccb=1-7&_nc_sid=75d36f&_nc_ohc=TlCztpaxbygQ7kNvgG9Iqn6&_nc_zt=23&_nc_ht=scontent.fsyd11-1.fna&_nc_gid=AYwaFSvxbBBT3KlbgOVDc1U&oh=00_AYC6aVx3Qlo-fxE8spGoEIAHE6rkAcguZivNCYKLy-d3hQ&oe=67182060',
-    link: 'https://www.facebook.com/events/903031551735450/',
+    title: "Rookie Code Rumble",
+    startTime: "Friday, 30 May 2025 18:00:00",
+    endTime: "Friday, 6 June 2025 23:59:59",
+    location: "Online (kick-off location TBD)",
+    image: "/images/events/rookie_code_rumble.jpg",
+    link: "https://www.facebook.com/events/1062484559164505"
   }
 ];
 
 export const previousEvents: eventInfo[] = [
   {
-    title: "Tech n' Tonic",
-    startTime: 'Monday, 14 Oct 2024 18:00:00',
-    endTime: 'Monday, 14 Oct 2024 20:00:00',
-    location: 'Hilton Hotel Sydney George St',
-    description: "Starry skies ✨ and business ties 👔 - are you ready for an evening of cocktails and connections at Tech 'n Tonic?",
-    image: 'https://scontent.fsyd11-2.fna.fbcdn.net/v/t39.30808-6/462109432_1042899677619643_4447945625462875474_n.jpg?stp=dst-jpg_s960x960&_nc_cat=102&ccb=1-7&_nc_sid=75d36f&_nc_ohc=LldgnyrrPzoQ7kNvgEkT9rZ&_nc_zt=23&_nc_ht=scontent.fsyd11-2.fna&_nc_gid=A4abttUIAM0znnZ5DhCSuH6&oh=00_AYCoja001nrw11TBm65CTUy7YeC7qUDzLQWZvPb8pAGXLw&oe=6718235B',
-    link: 'https://www.facebook.com/events/1078496197200589/'
+    title: "Flower Exchange",
+    startTime: "Wednesday, 23 Apr 2025 13:00:00",
+    endTime: "Wednesday, 23 Apr 2025 15:00:00",
+    location: "The Quad, UNSW",
+    image: "/images/events/flower_exchange.jpg",
+    link: "https://www.facebook.com/events/623535984011063"
   },
   {
-    title: "CSESoc Director Recruitment 2025",
-    startTime: 'Wednesday, 9 Oct 2024 23:00:00',
-    endTime: 'Wednesday, 16 Oct 2024 23:59:59',
-    location: 'Online',
-    description: 'OUR DIRECTOR RECRUITMENT APPLICATIONS HAVE OPENED 📝 📭',
-    image: 'https://scontent.fsyd11-1.fna.fbcdn.net/v/t39.30808-6/462491605_1047315693844708_8503531889535504643_n.jpg?stp=dst-jpg_s960x960&_nc_cat=106&ccb=1-7&_nc_sid=75d36f&_nc_ohc=lREBeWLY_uEQ7kNvgGzvZGT&_nc_zt=23&_nc_ht=scontent.fsyd11-1.fna&_nc_gid=AMGGaNEtlriGsleeiuMvJrs&oh=00_AYAeOSpSszxjzFvQsoLcT72TDooiJ27Ec9X-hhOU0RAnaA&oe=67183209',
-    link: 'https://www.facebook.com/events/882759546829988/'
+    title: "All the Stars Pubcrawl",
+    startTime: "Friday, 28 Mar 2025 19:00:00",
+    endTime: "Friday, 28 Mar 2025 23:30:00",
+    location: "Meet at Hyde Park",
+    image: "/images/events/all_the_stars.jpg",
+    link: "https://www.facebook.com/events/1171496917957222"
   },
   {
-    title: '2024 FMAA STEM Alternative Pathways Evening',
-    startTime: 'Wednesday, 9 Oct 2024 18:15:00',
-    endTime: 'Wednesday, 9 Oct 2024 20:30:00',
-    location: 'Roundhouse Room 3 & 4, UNSW',
-    description: 'Join us for an engaging panel discussion featuring representatives from Australia’s top firms across industries such as quantitative trading, management consulting, investment banking, asset management, and professional services. Gain valuable insights into how a STEM background can open doors to a variety of business and finance careers, and develop meaningful connections with industry professionals.',
-    image: 'https://scontent.fsyd14-1.fna.fbcdn.net/v/t39.30808-6/461261086_540208508398572_4261296480620587383_n.jpg?stp=dst-jpg_s960x960&_nc_cat=103&ccb=1-7&_nc_sid=75d36f&_nc_ohc=vf3IPe3_NPEQ7kNvgG_SxLX&_nc_zt=23&_nc_ht=scontent.fsyd14-1.fna&_nc_gid=ALtMsUxDU2QhcP8A5cS-tr7&oh=00_AYADt5ngFtSJsaT3YgKGjUHU8PHudDTb8eMHRu9JKuWuCw&oe=6713857F',
-    link: 'https://www.facebook.com/events/428430126479506',
-  },
-  {
-    title: 'Cultural Sweet Treats',
-    startTime: 'Wednesday, 9 Oct 2024 13:00:00',
-    endTime: 'Wednesday, 9 Oct 2024 15:00:00',
-    location: 'UNSW Quadrangle',
-    description: 'Looking for a creative way to celebrate diversity?🤔🌟 Come along for an afternoon of fun where you’ll get to design and decorate your own gift box 🎁 filled with cultural sweets from around the world!🌏 Try new flavours and treats as you craft your masterpiece for a friend, family member or yourself 😋',
-    image: 'https://scontent.fsyd14-1.fna.fbcdn.net/v/t39.30808-6/461677000_1040949901147954_3024711492723797720_n.jpg?stp=dst-jpg_s960x960&_nc_cat=100&ccb=1-7&_nc_sid=75d36f&_nc_ohc=zY_RqNGeg50Q7kNvgH4izQ8&_nc_zt=23&_nc_ht=scontent.fsyd14-1.fna&_nc_gid=AEzNe6W68jyyD37bJY9us2s&oh=00_AYAwuI1WXfZjhpeWSbHT1clhl05xVZdo0E-3nMCXo-Ndkw&oe=67139F71',
-    link: 'https://www.facebook.com/events/9261495757199727',
-  },
+    title: "Wheelchair Basketball Tournament",
+    startTime: "Sunday, 13 Apr 2025 13:00:00",
+    endTime: "Sunday, 13 Apr 2025 16:00:00",
+    location: "UNSW Village Green (Multi-purpose caged courts near Sam Cracknell Pavilion)",
+    image: "/images/events/wheelchair_basketball.jpg",
+    link: "https://www.facebook.com/events/3852672894949394"
+  }
 ];
