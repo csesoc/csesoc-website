db83bf834ff268dfbfca18f9d35130e32a0effae
Justine Kim <js.kim76@outlook.com>
2025-06-17 06:46:27 +1000

    combined execs and port data into single file

---
+++ frontend/public/data/members.ts
@@ -0,0 +1,21 @@
+export type Exec = {
+	name: string,
+	role: string,
+};
+
+export type PortfolioData = {
+	name: string,
+	description: string,
+	members: PortfolioMember[],
+};
+  
+export type PortfolioMember = {
+	name: string,
+	role: PortfolioRole,
+	imageUrl: string;
+}
+  
+export enum PortfolioRole {
+	DIRECTOR = "Director",
+	SUBCOM = "Subcommittee",
+}
