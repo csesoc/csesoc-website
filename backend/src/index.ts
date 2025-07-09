1f2ba564473aae9ffbba8d6d685d8ddf846fbea4
Yankai Zhu <29365738+someretical@users.noreply.github.com>
2025-07-09 13:15:19 +0800

    [I2-17] Added backend events API (#53)
    * Added backend events API
    
    * polished code
    
    ---------
    
    Co-authored-by: Yankai Zhu <z5482795@ad.unsw.edu.au>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,41 @@ import express, { Express } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import pingRoute from "./routes/ping";
+import eventsRoute from "./routes/events";
+import eventsWebhookRoute from "./routes/eventsWebhook";
+import { eventInfo, eventInfoMutex, fetchEvents } from "./data/eventData";
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT || 9000;
+(async () => {
+  try {
+    const events = await fetchEvents();
+    eventInfoMutex.runExclusive(() => eventInfo.concat(events));
+    console.log("Events fetched successfully");
+  } catch (error) {
+    // do we ungracefully bail out here???
+    // could just load from a backup file instead
+    console.error("Error fetching events on startup:", error);
+  }
 
-// Middleware
-app.use(express.json());
-app.use(cors());
+  const app: Express = express();
+  const port = process.env.PORT || 9000;
 
-app.use(pingRoute);
+  // Middleware
+  app.use(
+    express.json({
+      verify: (req, res, buf) => {
+        req.rawBody = buf;
+      },
+    })
+  );
+  app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server successfully started on port ${port}`);
-});
+  app.use(pingRoute);
+  app.use(eventsWebhookRoute);
+  app.use(eventsRoute);
+
+  app.listen(port, () => {
+    console.log(`Server successfully started on port ${port}`);
+  });
+})();
