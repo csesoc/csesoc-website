4ddb184bb46d214296c8364cccadca8948dab226
Therealtin <112870726+Therealtin@users.noreply.github.com>
2026-09-15 23:43:48 +1000

    feat | removed event carousel temporarlity

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
         </div>
 
         <AboutHomePage />
-        <EventsBrief />
+        {/* <EventsBrief /> */}
         <Sponsors />
       </section>
     </>
